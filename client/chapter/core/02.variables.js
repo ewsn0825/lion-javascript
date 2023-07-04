56395b77a9f5415f0895bc08cb224e8ee22f42b1
ewsn0825 <ewsn0825@naver.com>
2023-07-04 11:35:17 +0900

    변수와 스트릭트 모드 정리

---
+++ client/chapter/core/02.variables.js
@@ -0,0 +1,47 @@
+/* ------------------ */
+/* Variables          */
+/* ------------------ */
+
+let admin;
+let name;
+
+name='John';
+admin=name;
+// alert(admin);
+
+
+const ourPlanetName ="Earth";
+const currentOnlineUserName="LeeJaeHo";
+
+/* 다음 내용을 분석한 후, 프로그래밍 할 수 있도록 변수와 상수를 작성해봅니다. ----------- */
+
+// - 갯수 별 상품 가격 계산하기
+//calcProductPriceQuantity
+// - 구매 제품 가격의 총 합
+// totalProductPrice
+// - 1년 기준 일(day)자 수
+// DAYS_PER_YEAR
+// - 구매 결제 여부
+// isPayment
+// - 구매 결제 내역
+// paymentHistory
+// - 브랜드 접두사
+// brand_prefix="NIKE";
+// - 오늘의 운세
+// fortuneOfToday
+// - 상품 정보
+// productInfomation
+
+// variables
+let calcProductPriceQuantity;
+let totalProductPrice;
+let isPayment;
+let paymentHistory;
+let fortuneOfToday;
+
+// constant variables
+
+const DAYS_PER_YEAR =365;
+const productInfomation='';
+const BRAND_PREFIX="NIKE";
+
--- client/main.js
+++ client/main.js
@@ -1,5 +1,8 @@
-console.log('hello js!');
+/* console.log('hello js!');
 
 const a = 10;
 
 const b = 10;
+
+ */
+
