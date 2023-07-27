07157c07314a83291a37d905d3cd2a4131223476
ewsn0825 <ewsn0825@naver.com>
2023-07-27 09:19:03 +0900

    async await fetch 다루기

--- client/lib/utils/delay.js
+++ client/lib/utils/delay.js
@@ -1,11 +1,15 @@
 import { getNode } from '../dom/getNode.js';
+import { insertLast } from '../dom/insert.js';
+import { xhrPromise } from './xhr.js';
 
-function delay(callback, timeout = 1000) {
-  setTimeout(callback, timeout);
-}
+//callback 함수
 
-const first = getNode('.first');
-const second = getNode('.second');
+// function delay(callback, timeout = 1000) {
+//   setTimeout(callback, timeout);
+// }
+
+// const first = getNode('.first');
+// const second = getNode('.second');
 
 // delay(()=>{
 //   console.log(1);
@@ -24,15 +28,94 @@ const second = getNode('.second');
 // })
 
 //delayP 함수를 실행하면 리턴되는 값이 promise 객체입니다.
-function delayP() {
+
+// 객체 합성 mixin
+
+const defaultOptions = {
+  shouldReject: false,
+  timeout: 1000,
+  data: '성공!',
+  errorMessage: '알 수 없는 오류가 발생했습니다.',
+};
+
+function delayP(options) {
+  let config = { ...defaultOptions }; //원본 파괴를 막기 위해서 앝은 복사를 한다
+
+  if (typeof options === 'number') {
+    config.timeout = options;
+  }
+  //오버라이드
+  if (typeof options === 'object') {
+    config = { ...defaultOptions, ...options };
+  }
+
+  const { shouldReject, timeout, data, errorMessage } = config;
   return new Promise((resolve, reject) => {
-    resolve('성공입니다');
+    setTimeout(() => {
+      if (!shouldReject) {
+        resolve(data);
+      } else {
+        reject({ message: errorMessage });
+      }
+    }, timeout);
   });
 }
 
-delayP().then((res) => {
-  console.log(res);
-});
+// delayP({ shouldReject: false }).then((res) => {
+//   console.log(res);
+// });
+// .catch(({ message }) => {
+//   alert(message);
+// })
+// .finally(() => {
+//   console.log('암튼 실행됨');
+// });
 
 // console.log(2);
 // console.log(3);
+
+// async - 함수가 promise 객체를 반환 하도록
+//       - await 사용
+
+// await - 코드의 실행 흐름 제어 (멈춰)
+//       - result값 가져오기
+
+// async function 라면끓이기(){
+
+//   delayP({data:'물넣기'}).then((res)=>{
+//     console.log( res );
+//   })
+
+//   const 스프 = await delayP({data:'스프넣기'})
+//   console.log(스프);
+
+//   const 면 = await delayP({data:'면넣기'})
+//   console.log(면);
+
+//   const 계란 = await delayP({data:'계란넣기'})
+//   console.log(계란);
+
+//   const 접시 = await delayP({data:'접시'})
+//   console.log(접시);
+// }
+
+// 'https://jsonplaceholder.typicode.com/users'
+// 라면끓이기()
+
+async function getData(){
+
+  const data = xhrPromise.get('https://pokeapi.co/api/v2/pokemon/74')
+
+  // data.then((res)=>{
+  //   console.log( res );
+  // })
+  
+  const pokemon = await data;
+
+  // console.log( pokemon.sprites['front_default'] );
+
+  insertLast(document.body,`<img src="${pokemon.sprites['front_default']}" alt="" />`)
+
+}
+
+// getData()
--- client/main.js
+++ client/main.js
@@ -1,15 +1,22 @@
-import {xhr} from './lib/index.js';
+import { xhrPromise, tiger } from './lib/index.js';
 
+// const data = await tiger.get('https://jsonplaceholder.typicode.com/users');
 
-// xhr.get(
-//   'https://jsonplaceholder.typicode.com/users',
-//   (result) => {
-//     console.log(result);
-//   },
-//   (err) => {
-//     console.log(err);
-//   }
-// );
+const URL = 'https://jsonplaceholder.typicode.com/users';
 
+const pro = fetch(URL);
+// console.log(pro);
 
-//promise
+const response = await fetch(URL);
+// const data = await response.json();
+
+// console.log(await response.json());
+
+// fetch(URL).then((result)=>{
+
+//     result // response object
+//     return result.json()
+// })
+// .then((result)=>{
+//   console.log( result );
+// })
