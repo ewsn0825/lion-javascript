30dae02dbcaf7dd865cfc90db567973466d16846
ewsn0825 <ewsn0825@naver.com>
2023-07-27 18:13:06 +0900

    stotage 다루기

--- client/main.js
+++ client/main.js
@@ -1,5 +1,32 @@
-console.log('hello js!');
+import {
+  clearContents,
+  deleteStorage,
+  getNode,
+  getStorage,
+  setStorage,
+} from './lib/index.js';
 
-const a = 10;
+const textField = getNode('#textField');
+const btn = getNode('button');
 
-const b = 10;
+function handleTextField() {
+  const value = this.value;
+
+  setStorage('text', value);
+}
+
+function init() {
+  getStorage('text').then((res) => {
+    textField.value = res;
+    console.log(res);
+  });
+}
+
+function handleClear() {
+  clearContents('#textField');
+  deleteStorage();
+}
+
+textField.addEventListener('input', handleTextField);
+window.addEventListener('DOMContentLoaded', init);
+btn.addEventListener('click', handleClear);
